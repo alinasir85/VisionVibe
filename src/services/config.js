c63ef7c8f43c0bad98c3f39dacbe986d029a2c12
Ali Nasir <47711616+alinasir85@users.noreply.github.com>
2021-08-19 23:17:54 +0500

    VisionVibe

---
+++ src/services/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB5lp0gcllkh1jb_eXfnX2-FrO9rnid-kM",
+  authDomain: "visionvibe-cf851.firebaseapp.com",
+  databaseURL: "https://visionvibe-cf851-default-rtdb.firebaseio.com",
+  projectId: "visionvibe-cf851",
+  storageBucket: "visionvibe-cf851.appspot.com",
+  messagingSenderId: "975390389080",
+  appId: "1:975390389080:web:cab5794f4db926c9cb2b2d",
+  measurementId: "G-74DFKR00ZN"
+};
+
+export default firebaseConfig;
